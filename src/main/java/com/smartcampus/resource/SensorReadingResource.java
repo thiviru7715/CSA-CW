c912e360eafbec1ff7b599ebb8dcaa48a300ae89
Thiviru Igalawithana <thivirudamsith7715@gmail.com>
2026-04-14 06:30:49 +0530

    feat(readings): setup sub-resource locator and GET endpoint

--- src/main/java/com/smartcampus/resource/SensorReadingResource.java
+++ src/main/java/com/smartcampus/resource/SensorReadingResource.java
@@ -1,8 +1,16 @@
 package com.smartcampus.resource;
 
+import com.smartcampus.storage.DataStore;
+import com.smartcampus.model.SensorReading;
+import com.smartcampus.exception.SensorUnavailableException;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
 
 /**
  * Sub-resource for handling sensor readings.
@@ -14,9 +22,15 @@ import javax.ws.rs.core.MediaType;
 public class SensorReadingResource {
 
     private final String sensorId;
+    private final DataStore dataStore = DataStore.getInstance();
 
     public SensorReadingResource(String sensorId) {
         this.sensorId = sensorId;
+    }
+
+    @GET
+    public javax.ws.rs.core.Response getReadings() {
+        return javax.ws.rs.core.Response.ok(dataStore.getReadings(sensorId)).build();
     }
 
 }
